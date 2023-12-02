e2a758f4f766b1a9315ca5a0b72e8ea058896325
ciriti <ciriti@gmail.com>
2023-12-02 14:51:44 +0100

    action

---
+++ .github/actions/hello/index.js
@@ -0,0 +1,21 @@
+const core = require("@actions/core")
+const github = require("@actions/github")
+
+try{
+    const name = core.getInput("who_to_greet");
+
+    console.log(`Hello ${name}`);
+
+    const time = new Date();
+
+    core.setOutput("time", time.toTimeString());
+
+    core.exportVariable("HELLO_TIME", time);
+
+    core.startGroup("Loggin github contex");
+    console.log(JSON.stringify(github.context, null, 2));
+    core.endGroup();
+
+} catch (er) {
+    core.setFailed(er);
+}
